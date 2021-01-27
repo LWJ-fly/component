e04bbbcb415e08aa8ac0afc896578216124b8d5b
lwj <15518064076@163.com>
2021-01-28 01:19:55 +0800

    跨服务器上传图片暂时不太理解，后续继续学习

--- src/main/java/online/wenmeng/controller/FileUpLoadController.java
+++ src/main/java/online/wenmeng/controller/FileUpLoadController.java
@@ -1,5 +1,7 @@
 package online.wenmeng.controller;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,5 +39,28 @@ public class FileUpLoadController {
         }
         return "success";
     }
-
+    /**
+     * 跨服务器文件上传
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("fileuploadToImg")
+    public String fileuploadToImg(MultipartFile upload,String name){
+        String originalFilename = upload.getOriginalFilename();
+        originalFilename = UUID.randomUUID().toString().replace("-","")+originalFilename;
+        String path = "https://localhost:8080/upload";
+        File file = new File(path,originalFilename);
+        System.out.println(file.getAbsolutePath()+"\t\tname:"+name);
+        //创建客户端对象
+        Client client = Client.create();
+        //和图片服务器进行连接
+        WebResource webResource = client.resource(path + originalFilename);
+        //上传文件
+        try {
+            webResource.put(upload.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "success";
+    }
 }
